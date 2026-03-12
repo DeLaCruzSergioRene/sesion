8c44c4a166853291f7bcbfc943e9ea529aa1fbe9
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-03-12 10:49:11 -0600

    primeros cambios

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Bienvenido al incio de sesión."
-    page.padding = 20
+    page.padding = 25
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER 
 
     titulo = ft.Text(
         value="Inicio de Sesión.", 
         size=30, 
+        color=ft.Colors.BLUE_900,
         weight="bold", 
         text_align=ft.TextAlign.CENTER
     )
 
-    nombre = ft.TextField(label="Nombre completo")
-    correo = ft.TextField(label="Correo electrónico")
+    nombre = ft.TextField(label="Nombre completo", hint_text="Ingrese el nombre completo")
+    correo = ft.TextField(label="Correo electrónico", hint_text="Ingrese su correo electrónico")
+    edad = ft.TextField(label="Edad del usuario", hint_text="Ingrese su edad")
 
 
     resumen = ft.Text(value="", size=16, color=ft.Colors.BLUE_900)
@@ -22,6 +24,7 @@ def main(page: ft.Page):
         resumen.value = (
             f"Nombre: {nombre.value}\n"
             f"Correo: {correo.value}\n"
+            f"Edad: {edad.value}\n"
         )
         page.update()
 
@@ -40,9 +43,12 @@ def main(page: ft.Page):
                 ft.Row([titulo], alignment=ft.MainAxisAlignment.CENTER),
                 nombre,
                 correo,
+                edad,
+                ft.Row([boton_resumen], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Divider(),
                 resumen
             ],
-            spacing=15,
+            spacing=20,
             width=500
         )
     )
